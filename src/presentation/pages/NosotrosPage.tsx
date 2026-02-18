3e3ee2653830338ef1663a0b349c8523478f3585
SchuttValentin <schuttvalentin@gmail.com>
2026-02-18 11:37:18 -0300

    cambios en el front

--- src/presentation/pages/NosotrosPage.tsx
+++ src/presentation/pages/NosotrosPage.tsx
@@ -6,37 +6,24 @@ import PASBanner from "../components/ui/NosotrosUi/PASBanner";
 import TeamCarousel from "../components/ui/NosotrosUi/TeamCarousel";
 import VisionSection from "../components/ui/NosotrosUi/VisionSection";
 import Footer from '../components/ui/footer/Footer';
-import SumateSection from '../components/ui/NosotrosUi/SumateSection';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 export default function SegurosPage() {
 
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.hash) {
-      const el = document.querySelector(location.hash);
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  }, [location]);
 
 
-    return (
-        <>
-        <HeaderStaticNosotros/>
-        <div id="leer-mas">
-        <HistorySection/>
-        </div>
-        <PASBanner/>
-        <MissionSection/>
-        <CardsComponent/>
-        <VisionSection/>
-        <TeamCarousel/>
-        <SumateSection/>
-        <Footer/>
-        </>
+
+  return (
+    <>
+      <HeaderStaticNosotros />
+      <div id="leer-mas">
+        <HistorySection />
+      </div>
+      <PASBanner />
+      <MissionSection />
+      <CardsComponent />
+      <VisionSection />
+      <TeamCarousel />
+      <Footer />
+    </>
   )
 }
